1a2715c90100d15c5df20aaf83a5f5ee281b580a
Michael Large <m@codethebeard.com>
2016-08-08 21:33:51 -0400

    Adding new header

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * Haha, thought you would see jQuery here. Nah.
+ * I mean, it has it's uses, but damn. I REALLY don't need it here...
+ */
+
+window.addEventListener('load', function(){
+	var siteNav = document.querySelectorAll('.site__nav');
+	var siteHeader = document.querySelectorAll('.site__header');
+	var navToggle = document.getElementById('nav-toggle');
+
+	navToggle.addEventListener('click', function(){
+		this.classList.toggle('active');
+		siteNav[0].classList.toggle('active');
+		siteHeader[0].classList.toggle('active');
+	});
+});
