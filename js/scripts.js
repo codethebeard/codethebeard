83ed596007de7268be41ad445dcc3a9d24e55285
Michael Large <m@codethebeard.com>
2016-08-09 21:25:07 -0400

    Page transition scripts

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,37 @@ window.addEventListener('load', function(){
 	var siteNav = document.querySelectorAll('.site__nav');
 	var siteHeader = document.querySelectorAll('.site__header');
 	var navToggle = document.getElementById('nav-toggle');
+	var pageLink = document.querySelectorAll('.page-link');
+
+	window.setTimeout(function(){
+		siteHeader[0].classList.toggle('loaded');
+	}, 1000);
+
+	for (var i = 0; i < pageLink.length; i++) {
+		pageLink[i].addEventListener('click', preventClick, false);
+	}
+
+	function preventClick(event){
+		event.preventDefault();
+		console.log(event);
+		siteHeader[0].classList.toggle('loaded');
+		navToggle.classList.remove('active');
+		siteNav[0].classList.remove('active');
+		siteHeader[0].classList.remove('active');
+
+		window.setTimeout(function(){
+			var newLocation = event.srcElement.href;
+			console.log(newLocation);
+			if( newLocation != undefined ){
+				window.location.assign(newLocation);
+			} else {
+				newLocation = location.protocol + "//" + location.host;
+				window.location.assign(newLocation);
+			}
+		}, 1000);
+		return false;
+	}
+
 
 	navToggle.addEventListener('click', function(){
 		this.classList.toggle('active');
